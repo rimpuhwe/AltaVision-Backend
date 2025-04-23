cfdd8e8c9fae0aec5f501f3de7d7a16772ccc8ec
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-23 10:39:35 +0200

    add the database for our entire system

---
+++ config/db.js
@@ -0,0 +1,17 @@
+/* i will first plugging our apps to the database , once the app is started  our database will be swiched on too.
+ if  failed to connect it exit immediately
+*/
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); 
+  }
+};
+
+export default connectDB;
