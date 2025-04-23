7cf698cb87acd5866928355eb5c6c1fee6f0a7ea
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-04-23 10:43:03 +0200

    add comment for more understanding

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);// Mongo_URI is just  a palceholder , add up a link after seting up the environment 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
